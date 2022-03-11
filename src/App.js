5e0e859ce8d7bac1d467d2882eae5d9f51035554
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-11 11:31:59 +0100

    nav bar with link

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import { PageHome } from './pages/PageHome';
 import { PageReports } from './pages/PageReports';
 import { PageAbout } from './pages/PageAbout';
+import { Nav } from './components/Nav';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Website</h1>
+			<Nav/>
 			<div className="page">
 				<Routes>
 					<Route path="/" element={<PageHome />} />
