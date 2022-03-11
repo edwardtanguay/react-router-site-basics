d61e274e0af7bb539a8e08717c044e7d1f4cff27
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-11 11:36:58 +0100

    NavLink in

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const Nav = () => {
   return (
 	  <nav>
 		  <ul>
-			  <li><Link to="/">Home</Link></li>
-			  <li><Link to="/reports">Reports</Link></li>
-			  <li><Link to="/about">About</Link></li>
+			  <li><NavLink to="/">Home</NavLink></li>
+			  <li><NavLink to="/reports">Reports</NavLink></li>
+			  <li><NavLink to="/about">About</NavLink></li>
 		  </ul>
 	 </nav>
   )
