650ae7ab0964aa829660f648d227119180841de1
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-11 10:29:51 +0100

    pages showing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.scss';
 
+import { PageHome } from './pages/PageHome';
+import { PageReports } from './pages/PageReports';
+import { PageAbout } from './pages/PageAbout';
+
 function App() {
   return (
     <div className="App">
-		<p>Hello2</p>
+		<p>Hello</p>
+		<PageHome />
+		<PageReports />
+		<PageAbout />
     </div>
   );
 }
